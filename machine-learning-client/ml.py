a79fcd9755f1a79ca3a97de68a404dcd44dc1126
gmp9469 <83371057+gmp9469@users.noreply.github.com>
2023-11-22 22:46:01 -0500

    speech recognition and response client

---
+++ machine-learning-client/ml.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+from pymongo import MongoClient
+import pyaudio as pa
+
+# MongoDB connection
+"""client = MongoClient('')
+db = client['']
+collection = db['']"""
+
+recognizer = sr.Recognizer()
+
+def capture_voice_input():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    return audio
+
+def convert_voice_to_text(audio):
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        text = ""
+        print("Sorry, I didn't understand that.")
+    except sr.RequestError as e:
+        text = ""
+        print("Error; {0}".format(e))
+    return text
+
+def process_voice_command(text):
+    if "human" in text.lower():
+        print("hi")
+    elif "cat" in text.lower():
+        print("meow")
+    elif "dog" in text.lower():
+        print("woof")
+    elif "cow" in text.lower():
+        print("moo")
+    elif "bird" in text.lower():
+        print("chirp")
+    elif "frog" in text.lower():
+        print("ribbit")
+    elif "snake" in text.lower():
+        print("hisss")
+    elif "goodbye" in text.lower():
+        print("Goodbye! Have a great day!")
+        return True
+    else:
+        print("I didn't understand that command. Please try again.")
+    return False
+
+def main():
+    end_program = False
+    while not end_program:
+        audio = capture_voice_input()
+        text = convert_voice_to_text(audio)
+        end_program = process_voice_command(text)
+
+if __name__ == "__main__":
+    main()
