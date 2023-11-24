56936eb0777baa889f51c1d9f27441bd9824e81c
dj9771 <yj2363@nyu.edu>
2023-11-23 23:30:12 -0500

    write test_ml

--- machine-learning-client/test_ml.py
+++ machine-learning-client/test_ml.py
@@ -1,19 +1,54 @@
-import pytest
 import speech_recognition as sr
-from unittest import mock
-from unittest.mock import Mock, patch, mock_open
-from unittest.mock import MagicMock
-from io import StringIO
+import pytest
+from unittest.mock import MagicMock, patch
 
 from ml import (
     capture_voice_input,
-    convert_voice_to_text,
     process_voice_command,
-    sr as real_sr,
 )
 
 recognizer = sr.Recognizer()
 
-import speech_recognition as sr
 
-recognizer = sr.Recognizer()
+def test_capture_voice_input_timeout():
+    # Mocking the listen function to simulate a WaitTimeoutError
+    def listen_mock(*args, **kwargs):
+        raise sr.WaitTimeoutError()
+
+    # Patching the listen function with the mocked one
+    with patch.object(sr.Recognizer, "listen", side_effect=listen_mock):
+        # Test with timeout
+        result = capture_voice_input(timeout=0.01)
+        assert result is None
+
+
+def test_capture_voice_input():
+    # Create a mock AudioData object
+    mock_audio_data = sr.AudioData(
+        b"fake_audio_data", 16000, 2
+    )  # Replace with relevant AudioData initialization
+
+    # Mock Recognizer class with a mock listen method
+    mock_recognizer_instance = MagicMock()
+    mock_listen = MagicMock(return_value=mock_audio_data)
+    setattr(mock_recognizer_instance, "listen", mock_listen)
+
+    # Patch the Recognizer class in the imported module
+    with patch("ml.sr.Recognizer", return_value=mock_recognizer_instance):
+        # Call the function under test
+        result = capture_voice_input()
+
+        # Check that the result is an instance of AudioData
+        assert isinstance(result, type(mock_audio_data))
+
+
+def test_process_voice_command_human():
+    # Test with recognized animal
+    result = process_voice_command("human")
+    assert result is False
+
+
+# Other test cases for different scenarios can be added similarly
+
+if __name__ == "__main__":
+    pytest.main()
