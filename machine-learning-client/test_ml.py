2a2d1a83f03414e7aa5ddf19ce0cbde7c6d2521d
dj9771 <yj2363@nyu.edu>
2023-11-23 23:44:02 -0500

    docstring

--- machine-learning-client/test_ml.py
+++ machine-learning-client/test_ml.py
@@ -10,46 +10,39 @@ from ml import (
 
 recognizer = sr.Recognizer()
 
-"""Module providing a function printing python version."""
+
 def test_capture_voice_input_timeout():
-    # Mocking the listen function to simulate a WaitTimeoutError
+    """Mocking the listen function to simulate a WaitTimeoutError"""
+
     def listen_mock(*args, **kwargs):
+        """Simulating Wait time out Error"""
         raise sr.WaitTimeoutError()
 
-    # Patching the listen function with the mocked one
     with patch.object(sr.Recognizer, "listen", side_effect=listen_mock):
-        # Test with timeout
         result = capture_voice_input(timeout=0.01)
         assert result is None
 
-"""Module providing a function printing python version."""
-def test_capture_voice_input():
-    # Create a mock AudioData object
-    mock_audio_data = sr.AudioData(
-        b"fake_audio_data", 16000, 2
-    )  # Replace with relevant AudioData initialization
 
-    # Mock Recognizer class with a mock listen method
+def test_capture_voice_input():
+    """Test with mock voice input capture"""
+
+    mock_audio_data = sr.AudioData(b"fake_audio_data", 16000, 2)
+
     mock_recognizer_instance = MagicMock()
     mock_listen = MagicMock(return_value=mock_audio_data)
     setattr(mock_recognizer_instance, "listen", mock_listen)
 
-    # Patch the Recognizer class in the imported module
     with patch("ml.sr.Recognizer", return_value=mock_recognizer_instance):
-        # Call the function under test
         result = capture_voice_input()
 
-        # Check that the result is an instance of AudioData
         assert isinstance(result, type(mock_audio_data))
 
-"""Module providing a function printing python version."""
+
 def test_process_voice_command_human():
-    # Test with recognized animal
+    """Test with recognized command"""
     result = process_voice_command("human")
     assert result is False
 
-
-# Other test cases for different scenarios can be added similarly
 
 if __name__ == "__main__":
     pytest.main()
