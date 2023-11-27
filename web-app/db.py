9b4c6d48e1f110f84c18b01c30473d2fee11003e
dj9771 <yj2363@nyu.edu>
2023-11-27 13:30:55 -0500

    solve pylint

--- web-app/db.py
+++ web-app/db.py
@@ -2,7 +2,13 @@
 from flask_pymongo import pymongo
 
 
-CONNECTION_STRING = "mongodb+srv://exz209:06bW5AA1Z2X9YeON@cluster0.apy9mup.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = (
+    "mongodb+srv://exz209:"
+    "06bW5AA1Z2X9YeON@"
+    "cluster0.apy9mup.mongodb.net/"
+    "?retryWrites=true&w=majority"
+)
+
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database("animals_db")
 user_collection = pymongo.collection.Collection(db, "user_collection")
