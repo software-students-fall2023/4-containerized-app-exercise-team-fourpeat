b377e46ea24fc13edceb77bdfe62628aa4e0cbc5
dj9771 <yj2363@nyu.edu>
2023-11-27 13:05:51 -0500

    Add docstring

--- web-app/app.py
+++ web-app/app.py
@@ -1,3 +1,4 @@
+"""Web app"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Return the main page"""
     return "Hello, Flask!"
 
 
