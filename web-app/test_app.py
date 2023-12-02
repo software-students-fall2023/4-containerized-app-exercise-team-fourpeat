35ed4a59a86102a13d31f1d5ed1fa6ec0595cc91
Emma <emz.zheng@gmail.com>
2023-12-02 13:40:21 -0500

    reformat test_app.py with black

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -2,17 +2,20 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     """Client"""
     with app.test_client() as client:
         yield client
 
+
 def test_home_route(client):
     """Test the home route."""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Animal Sounds" in response.data
+
 
 def test_run_route(client):
     """Test the /run route."""
