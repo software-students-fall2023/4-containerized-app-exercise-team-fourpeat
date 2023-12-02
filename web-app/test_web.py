7f5955ff96d28364d8bebf447b1754f1dfec6c94
gmp9469 <83371057+gmp9469@users.noreply.github.com>
2023-12-02 14:29:24 -0500

    Create test_web.py

---
+++ web-app/test_web.py
@@ -0,0 +1,36 @@
+"""Unit tests of functions in app.py"""
+from unittest.mock import Mock, MagicMock, patch
+import speech_recognition as sr
+import pytest
+
+from app import (
+  capture_voice_input,
+)
+
+recognizer = sr.Recognizer()
+
+def test_capture_voice_input_timeout():
+    """Test of timeout in voice input capture"""
+
+    def listen_mock(*args, **kwargs):
+        """Simulating wait timeout error"""
+        raise sr.WaitTimeoutError()
+
+    with patch.object(sr.Recognizer, "listen", side_effect=listen_mock):
+        result = capture_voice_input(timeout=0.01)
+        assert result is None
+
+
+def test_capture_voice_input():
+    """Test to mock voice input capture"""
+
+    mock_audio_data = sr.AudioData(b"fake_audio_data", 16000, 2)
+
+    mock_recognizer_instance = MagicMock()
+    mock_listen = MagicMock(return_value=mock_audio_data)
+    setattr(mock_recognizer_instance, "listen", mock_listen)
+
+    with patch("ml.sr.Recognizer", return_value=mock_recognizer_instance):
+        result = capture_voice_input()
+
+        assert isinstance(result, type(mock_audio_data))
