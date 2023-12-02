01e117420420a37d0db664d219fd92808592be08
Emma <emz.zheng@gmail.com>
2023-12-02 14:12:03 -0500

    unit tests for web app

---
+++ web-app/test_app.py
@@ -0,0 +1,23 @@
+"""Unit tests for the Flask web application."""
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    """Client"""
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_route(client):
+    """Test the home route."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Animal Sounds" in response.data
+
+
+def test_run_route(client):
+    """Test the /run route."""
+    response = client.get("/run")
+    assert response.status_code == 302
