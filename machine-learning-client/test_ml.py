b976fc1aa4f80413e74316367310b288ae1f5eea
dj9771 <yj2363@nyu.edu>
2023-12-04 03:36:49 -0500

    test

--- machine-learning-client/test_ml.py
+++ machine-learning-client/test_ml.py
@@ -3,10 +3,25 @@ from unittest.mock import Mock, patch
 import speech_recognition as sr
 import pytest
 
-from ml import convert_voice_to_text
-
+from ml import (
+    capture_voice_input,
+    convert_voice_to_text,
+    process_voice_command,
+)
 
 recognizer = sr.Recognizer()
+
+
+def test_capture_voice_input_timeout():
+    """Test of timeout in voice input capture"""
+
+    def listen_mock(*args, **kwargs):
+        """Simulating wait timeout error"""
+        raise sr.WaitTimeoutError()
+
+    with patch.object(sr.Recognizer, "listen", side_effect=listen_mock):
+        result = capture_voice_input(timeout=0.01)
+        assert result is None
 
 
 def test_convert_voice_to_text():
@@ -44,6 +59,73 @@ def test_convert_voice_to_text_request_error():
         result = convert_voice_to_text(mock_audio)
 
         assert result == ""
+
+
+@patch("ml.save_to_database")
+def test_process_voice_command(mock_save_to_db):
+    """Test for different strings"""
+    texts = [
+        "Hello, human!",
+        "meow",
+        "Dog barks loudly",
+        "cow says moo",
+        "No match",
+        "Goodbye!",
+    ]
+
+    # Call the method for each text
+    for text in texts:
+        process_voice_command(text)
+
+    # Assertions
+    assert mock_save_to_db.call_count == len(
+        texts
+    )  # Check if save_to_database is called same times as texts
+
+
+@patch("ml.save_to_database")
+def test_process_voice_command_human(mock_save_to_db):
+    """Test for different strings"""
+    texts = "Hello, human!"
+
+    # Call the method for each text
+
+    process_voice_command(texts)
+
+    # Assertions
+    assert (
+        mock_save_to_db.call_count == 1
+    )  # Check if save_to_database is called same times as texts
+
+
+@patch("ml.save_to_database")
+def test_process_voice_command_cat(mock_save_to_db):
+    """Test for different strings"""
+    texts = "cat"
+
+    # Call the method for each text
+
+    process_voice_command(texts)
+
+    # Assertions
+    assert (
+        mock_save_to_db.call_count == 1
+    )  # Check if save_to_database is called same times as texts
+
+
+@patch("ml.save_to_database")
+def test_process_voice_command_dog(mock_save_to_db):
+    """Test for different strings"""
+    texts = "dog"
+
+    # Call the method for each text
+
+    process_voice_command(texts)
+
+    # Assertions
+    assert (
+        mock_save_to_db.call_count == 1
+    )  # Check if save_to_database is called same times as texts
 
 
 if __name__ == "__main__":
