e028d74a6a0ce9ae6537fc87ec5347b66afe0249
meganchen99 <myc9078@nyu.edu>
2023-12-04 02:02:21 -0500

    changed database access method for docker

--- machine-learning-client/animal_db.py
+++ machine-learning-client/animal_db.py
@@ -1,7 +1,9 @@
 """Database for this application"""
 from pymongo import MongoClient
+import os
 
-CONNECTION_STRING = "mongodb://localhost:27017/"
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+CONNECTION_STRING = f"mongodb://{DATABASE_HOST}:27017/"
 
 client = MongoClient(CONNECTION_STRING)
 db = client["database1"]
