6a36267a5424e5b7e0c1dcbd40daff47d388b1dd
dj9771 <yj2363@nyu.edu>
2023-12-04 04:39:54 -0500

    black

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -4,6 +4,7 @@ import pytest
 from app import app
 from flask import template_rendered
 
+
 @pytest.fixture
 def client():
     """Client"""
@@ -23,7 +24,7 @@ def test_animals_db_route(client):
     template_rendered.connect(record, app)
 
     # Perform a GET request to the animals_db route
-    with client.get('/') as response:
+    with client.get("/") as response:
         pass  # Use the response if needed, for example, response.status_code
 
     # Disconnect the signal after the request is done
@@ -32,8 +33,9 @@ def test_animals_db_route(client):
     # Verify that the template was rendered with the 'animals' variable
     assert len(recorded) == 1  # Ensure one template was rendered
     template, context = recorded[0]
-    assert template.name == 'index.html'  # Check the rendered template name
-    assert 'animals' in context  # Check if 'animals' variable exists in the context
+    assert template.name == "index.html"  # Check the rendered template name
+    assert "animals" in context  # Check if 'animals' variable exists in the context
+
 
 def test_capture_audio_endpoint(client, monkeypatch):
     """Define the data payload to simulate POST request"""
